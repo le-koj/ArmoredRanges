24c597663646423625081150223ba7cca8b2343d
Le Koj <mitchkojelegance@gmail.com>
2018-07-18 04:58:06 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+###############################################
+### FILE: python module                     ###
+###                                         ###
+### DESCRIPTION: main backend module        ###
+###             for armoredrange.com webapp ###
+###                                         ###
+### PURPOSE: respond/handle request towards ###
+###          app.                           ###
+###############################################
+
+import webapp2, os, jinja2
